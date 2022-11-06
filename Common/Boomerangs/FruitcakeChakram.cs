1245ced638aa7828c0c8f64b48302942ea8ae9a6
stormytuna <stormytuna@outlook.com>
2022-11-06 15:06:52 +0000

    Change fruit chakram to be a acquired from tree shake

--- Common/Boomerangs/FruitcakeChakram.cs
+++ Common/Boomerangs/FruitcakeChakram.cs
@@ -1,12 +1,15 @@
+using Bangarang.Common.Configs;
 using Bangarang.Common.Systems;
+using Microsoft.Xna.Framework;
 using System.Collections.Generic;
 using Terraria;
+using Terraria.Enums;
 using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace Bangarang.Common.Boomerangs {
     public class FruitcakeChakramGI : GlobalItem {
-        public override bool AppliesToEntity(Item entity, bool lateInstantiation) => entity.type == ItemID.FruitcakeChakram;
+        public override bool AppliesToEntity(Item entity, bool lateInstantiation) => entity.type == ItemID.FruitcakeChakram && ServerConfig.Instance.VanillaChanges;
 
         public override void ModifyTooltips(Item item, List<TooltipLine> tooltips) {
             int index = tooltips.FindLastIndex(t => t.Mod == "Terraria");
@@ -16,7 +19,7 @@ namespace Bangarang.Common.Boomerangs {
     }
 
     public class FruitcakeChakramGP : GlobalProjectile {
-        public override bool AppliesToEntity(Projectile entity, bool lateInstantiation) => entity.type == ProjectileID.FruitcakeChakram;
+        public override bool AppliesToEntity(Projectile entity, bool lateInstantiation) => entity.type == ProjectileID.FruitcakeChakram && ServerConfig.Instance.VanillaChanges;
 
         public override void OnHitNPC(Projectile projectile, NPC target, int damage, float knockback, bool crit) {
             int[] buffs = ArraySystem.FruitcakeChakramDebuffs;
@@ -29,12 +32,59 @@ namespace Bangarang.Common.Boomerangs {
         }
     }
 
-    public class FruitcakeCharkramGNPC : GlobalNPC {
-        public override bool AppliesToEntity(NPC entity, bool lateInstantiation) => entity.type == NPCID.Merchant;
+    public class FruitcakeChakramDetour : ModSystem {
+        public override void Load() {
+            if (ServerConfig.Instance.VanillaChanges) {
+                On.Terraria.WorldGen.ShakeTree += WorldGen_ShakeTree;
+            }
+        }
+        public override void Unload() {
+            if (ServerConfig.Instance.VanillaChanges) {
+                On.Terraria.WorldGen.ShakeTree -= WorldGen_ShakeTree;
+            }
+        }
 
-        public override void SetupShop(int type, Chest shop, ref int nextSlot) {
-            if (Main.dayTime) {
-                Helpers.AddToShop(ref shop, ref nextSlot, ItemID.FruitcakeChakram, i => i.type == ItemID.Shuriken, 12);
+        private void WorldGen_ShakeTree(On.Terraria.WorldGen.orig_ShakeTree orig, int i, int j) {
+            orig(i, j);
+
+            // Gets our trees bottom tile
+            // Copied from vanilla: Terraria.Worldgen.cs:40444
+            int x = i;
+            int y = j;
+            Tile tileSafely = Framing.GetTileSafely(x, y);
+            if (tileSafely.TileType == 323) {
+                while (y < Main.maxTilesY - 50 && (!tileSafely.HasTile || tileSafely.TileType == 323)) {
+                    y++;
+                    tileSafely = Framing.GetTileSafely(x, y);
+                }
+
+                return;
+            }
+
+            int num = tileSafely.TileFrameX / 22;
+            int num2 = tileSafely.TileFrameY / 22;
+            if (num == 3 && num2 <= 2)
+                x++;
+            else if (num == 4 && num2 >= 3 && num2 <= 5)
+                x--;
+            else if (num == 1 && num2 >= 6 && num2 <= 8)
+                x--;
+            else if (num == 2 && num2 >= 6 && num2 <= 8)
+                x++;
+            else if (num == 2 && num2 >= 9)
+                x++;
+            else if (num == 3 && num2 >= 9)
+                x--;
+
+            tileSafely = Framing.GetTileSafely(x, y);
+            while (y < Main.maxTilesY - 50 && (!tileSafely.HasTile || TileID.Sets.IsATreeTrunk[tileSafely.TileType] || tileSafely.TileType == 72)) {
+                y++;
+                tileSafely = Framing.GetTileSafely(x, y);
+            }
+
+            var treeType = WorldGen.GetTreeType(Main.tile[x, y].TileType);
+            if (WorldGen.genRand.NextBool(15) && treeType == TreeTypes.Snow) {
+                Item.NewItem(WorldGen.GetItemSource_FromTreeShake(i, j), new Rectangle(i * 16, j * 16, 16, 16), ItemID.FruitcakeChakram);
             }
         }
     }
