0ceb6684e6e692fd8b8624a707e6f836f4ac7fae
stormytuna <stormytuna@outlook.com>
2022-11-06 15:07:14 +0000

    Change bloody machete to be sold by skeleton merchant

--- Common/Boomerangs/BloodyMachete.cs
+++ Common/Boomerangs/BloodyMachete.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using Bangarang.Common.Configs;
+using Microsoft.Xna.Framework;
 using System.Collections.Generic;
 using Terraria;
 using Terraria.ID;
@@ -6,7 +7,7 @@ using Terraria.ModLoader;
 
 namespace Bangarang.Common.Boomerangs {
     public class BloodyMacheteGI : GlobalItem {
-        public override bool AppliesToEntity(Item entity, bool lateInstantiation) => entity.type == ItemID.BloodyMachete;
+        public override bool AppliesToEntity(Item entity, bool lateInstantiation) => entity.type == ItemID.BloodyMachete && ServerConfig.Instance.VanillaChanges;
 
         public override void ModifyTooltips(Item item, List<TooltipLine> tooltips) {
             int index = tooltips.FindLastIndex(t => t.Mod == "Terraria");
@@ -15,7 +16,7 @@ namespace Bangarang.Common.Boomerangs {
     }
 
     public class BloodyMacheteGP : GlobalProjectile {
-        public override bool AppliesToEntity(Projectile entity, bool lateInstantiation) => entity.type == ProjectileID.BloodyMachete;
+        public override bool AppliesToEntity(Projectile entity, bool lateInstantiation) => entity.type == ProjectileID.BloodyMachete && ServerConfig.Instance.VanillaChanges;
 
         public override void ModifyHitNPC(Projectile projectile, NPC target, ref int damage, ref float knockback, ref bool crit, ref int hitDirection) {
             // Bloody Machete uses ai[0] as a frame counter in vanilla so we can just reuse that
@@ -26,12 +27,13 @@ namespace Bangarang.Common.Boomerangs {
         }
     }
 
-    public class BloodyMacheteCharkramGNPC : GlobalNPC {
-        public override bool AppliesToEntity(NPC entity, bool lateInstantiation) => entity.type == NPCID.Merchant;
+    public class BloodyMacheteGNPC : GlobalNPC {
+        public override bool AppliesToEntity(NPC entity, bool lateInstantiation) => ServerConfig.Instance.VanillaChanges;
 
         public override void SetupShop(int type, Chest shop, ref int nextSlot) {
-            if (!Main.dayTime) {
-                Helpers.AddToShop(ref shop, ref nextSlot, ItemID.BloodyMachete, i => i.type == ItemID.Shuriken, 12);
+            if (type == NPCID.SkeletonMerchant && Main.moonPhase <= 3) {
+                shop.item[nextSlot].SetDefaults(ItemID.BloodyMachete);
+                nextSlot++;
             }
         }
     }
