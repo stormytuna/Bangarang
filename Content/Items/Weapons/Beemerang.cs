927c7cd657bd9e4db5266ea5c31b947ae9185e5c
stormytuna <stormytuna@outlook.com>
2022-11-07 10:40:10 +0000

    Add beemerang

---
+++ Content/Items/Weapons/Beemerang.cs
@@ -0,0 +1,46 @@
+using Bangarang.Common.Players;
+using Bangarang.Content.Projectiles.Weapons;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Bangarang.Content.Items.Weapons {
+    public class Beemerang : ModItem {
+        public override void SetStaticDefaults() {
+            Tooltip.SetDefault("Releases bees on impact");
+        }
+
+        public override void SetDefaults() {
+            Item.width = 24;
+            Item.height = 46;
+            Item.rare = ItemRarityID.Orange;
+            Item.value = Item.sellPrice(gold: 2);
+
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.useAnimation = 15;
+            Item.useTime = 15;
+            Item.autoReuse = true;
+            Item.UseSound = SoundID.Item1;
+            Item.noMelee = true;
+            Item.noUseGraphic = true;
+
+            Item.shoot = Projectile;
+            Item.shootSpeed = 13f;
+            Item.damage = 30;
+            Item.crit = 6;
+            Item.knockBack = 8f;
+            Item.DamageType = DamageClass.Melee;
+        }
+
+        public override void AddRecipes() {
+            CreateRecipe()
+                .AddIngredient(ItemID.BeeWax, 14)
+                .AddTile(TileID.Anvils)
+                .Register();
+        }
+
+        public int Projectile { get => ModContent.ProjectileType<BeemerangProj>(); }
+
+        public override bool CanUseItem(Player player) => player.ownedProjectileCounts[Projectile] < player.GetModPlayer<BangarangPlayer>().ExtraBoomerangs + 1;
+    }
+}
---
+++ Content/Projectiles/Weapons/BeemerangProj.cs
@@ -0,0 +1,40 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace Bangarang.Content.Projectiles.Weapons {
+    public class BeemerangProj : Boomerang {
+        public override void SetStaticDefaults() {
+            DisplayName.SetDefault("Beemerang");
+        }
+
+        public override void SetDefaults() {
+            Projectile.width = 36;
+            Projectile.height = 36;
+            Projectile.aiStyle = -1;
+
+            Projectile.DamageType = DamageClass.Melee;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.penetrate = -1;
+
+            Projectile.tileCollide = true;
+
+            ReturnSpeed = 14f;
+            HomingOnOwnerStrength = 4f;
+            TravelOutFrames = 30;
+            DoTurn = true;
+        }
+
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit) {
+            int numBees = Main.rand.Next(3);
+            for (int i = 0; i < numBees; i++) {
+                Vector2 velocity = new(Main.rand.NextFloat(0f, 0.3f), 0f);
+                velocity = velocity.RotatedByRandom(MathHelper.TwoPi);
+                Projectile.NewProjectileDirect(Projectile.GetSource_FromAI(), Projectile.Center, velocity, Owner.beeType(), Owner.beeDamage(damage), Owner.beeKB(knockback), Projectile.owner);
+            }
+
+            base.OnHitNPC(target, damage, knockback, crit);
+        }
+    }
+}
