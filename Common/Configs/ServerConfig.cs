0f4d0022112f74f01ba2fc387a743bac891cf17e
stormytuna <stormytuna@outlook.com>
2022-11-08 22:56:19 +0000

    Update ServerConfig.cs

--- Common/Configs/ServerConfig.cs
+++ Common/Configs/ServerConfig.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.ComponentModel;
 using Terraria.ModLoader.Config;
 
@@ -6,6 +7,8 @@ namespace Bangarang.Common.Configs {
         public static ServerConfig Instance;
 
         public override ConfigScope Mode => ConfigScope.ServerSide;
+
+        [Header("General")]
 
         [Label("Vanilla boomerang changes")]
         [DefaultValue(true)]
@@ -18,5 +21,17 @@ namespace Bangarang.Common.Configs {
         [Label("Modded accessories")]
         [DefaultValue(true)]
         public bool ModdedAccessory { get; set; }
+
+
+        [Header("Auto-compatibility")]
+
+        [Label("Automatically support other mods boomerangs")]
+        [Tooltip("This will use data from the projectile to guess which projectiles are boomerangs\nThis may have some unintended behaviour with some mods")]
+        [DefaultValue(false)]
+        public bool AssumeModdedBoomerangs { get; set; }
+
+        [Label("Auto-support item blacklist")]
+        [Tooltip("Adding an item to this list will prevent this mod from assuming it's a boomerang\nOnly add other mods boomerangs to this list")]
+        public List<ItemDefinition> AssumeModdedBoomerangsBlacklist { get; set; } = new();
     }
 }
