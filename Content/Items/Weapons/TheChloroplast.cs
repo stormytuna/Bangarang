0b619dc5d02504e4c425847a5a1d5fd5b5c16a91
stormytuna <stormytuna@outlook.com>
2022-11-08 22:58:20 +0000

    Add chloroplast

---
+++ Content/Items/Weapons/TheChloroplast.cs
@@ -0,0 +1,45 @@
+using Bangarang.Common.Players;
+using Bangarang.Content.Projectiles.Weapons;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Bangarang.Content.Items.Weapons {
+    public class TheChloroplast : ModItem {
+        public override void SetStaticDefaults() {
+            Tooltip.SetDefault("Bursts into stingers at its apex");
+        }
+
+        public override void SetDefaults() {
+            Item.width = 54;
+            Item.height = 54;
+            Item.rare = ItemRarityID.Lime;
+            Item.value = Item.sellPrice(gold: 5);
+
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.useAnimation = 30;
+            Item.useTime = 30;
+            Item.autoReuse = true;
+            Item.UseSound = SoundID.Item1;
+            Item.noMelee = true;
+            Item.noUseGraphic = true;
+
+            Item.shoot = Projectile;
+            Item.shootSpeed = 20;
+            Item.damage = 50;
+            Item.knockBack = 12f;
+            Item.DamageType = DamageClass.Melee;
+        }
+
+        public int Projectile { get => ModContent.ProjectileType<TheChloroplastProj>(); }
+
+        public override bool CanUseItem(Player player) => player.ownedProjectileCounts[Projectile] < player.GetModPlayer<BangarangPlayer>().ExtraBoomerangs + 1;
+
+        public override void AddRecipes() {
+            CreateRecipe()
+                .AddIngredient(ItemID.ChlorophyteBar, 14)
+                .AddTile(TileID.MythrilAnvil)
+                .Register();
+        }
+    }
+}
---
+++ Content/Projectiles/Weapons/TheChloroplastProj.cs
@@ -0,0 +1,82 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Bangarang.Content.Projectiles.Weapons {
+    public class TheChloroplastProj : Boomerang {
+        public override void SetStaticDefaults() {
+            DisplayName.SetDefault("The Chloroplast");
+        }
+
+        public override void SetDefaults() {
+            Projectile.width = 54;
+            Projectile.height = 54;
+            Projectile.aiStyle = -1;
+
+            Projectile.DamageType = DamageClass.Melee;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.penetrate = -1;
+            Projectile.tileCollide = true;
+
+            //ReturnSpeed = 12f;
+            //HomingOnOwnerStrength = 3f;
+            TravelOutFrames = 60;
+            Rotation = 0.3f;
+            DoTurn = true;
+        }
+
+        public override void AI() {
+            // Using ai[0] as our "stay in place" mode
+            Projectile.velocity /= 1.05f;
+            Rotation /= 1.02f;
+
+            // Dust
+            for (int i = 0; i < 2; i++) {
+                Dust d = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.ChlorophyteWeapon, 0f, 0f, 0, default, Main.rand.NextFloat(0.8f, 1.2f));
+                d.noGravity = true;
+            }
+
+            base.AI();
+        }
+
+        public override void OnReachedApex() {
+            Projectile.Kill();
+        }
+
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit) {
+
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity) => false;
+
+        public override void Kill(int timeLeft) {
+            // Smart fire at targets
+            // Get our list of nearby enemies
+            var closeNPCs = Helpers.GetNearbyEnemies(Projectile.Center, 30f * 16f, true, false);
+
+            for (int i = 0; i < 3; i++) {
+                bool isRandom = i < 3 - closeNPCs.Count;
+                Vector2 velocity = isRandom ? new(0f, 1f) : closeNPCs[i].Center - Projectile.Center;
+                velocity.Normalize();
+                if (isRandom) {
+                    velocity = velocity.RotatedByRandom(MathHelper.TwoPi);
+                }
+                velocity *= Main.rand.NextFloat(16f, 20f);
+                Projectile stinger = Projectile.NewProjectileDirect(Projectile.GetSource_FromThis(), Projectile.Center, velocity, ProjectileID.Stinger, Projectile.damage / 2, Projectile.knockBack / 3f, Projectile.owner);
+                stinger.DamageType = DamageClass.Melee;
+                stinger.hostile = false;
+                stinger.friendly = true;
+                stinger.timeLeft = 5 * 60;
+            }
+
+            // Effects
+            for (int i = 0; i < 24; i++) {
+                Dust d = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.ChlorophyteWeapon, 0f, 0f, 0, default, Main.rand.NextFloat(1.4f, 1.8f));
+                d.noGravity = true;
+                d.velocity *= 6f;
+            }
+        }
+    }
+}
