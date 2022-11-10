8ecdbaa9089c0a570d41e8565f217c7d7e67265e
stormytuna <stormytuna@outlook.com>
2022-11-10 09:58:29 +0000

    Add white dwarf

---
+++ Content/Items/Weapons/WhiteDwarf.cs
@@ -0,0 +1,45 @@
+using Bangarang.Common.Players;
+using Bangarang.Content.Projectiles.Weapons;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Bangarang.Content.Items.Weapons {
+    public class WhiteDwarf : ModItem {
+        public override void SetStaticDefaults() {
+            Tooltip.SetDefault("Incinerates enemies with stellar energy");
+        }
+
+        public override void SetDefaults() {
+            Item.width = 14;
+            Item.height = 14;
+            Item.rare = ItemRarityID.Red;
+            Item.value = Item.sellPrice(gold: 10);
+
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.useAnimation = 15;
+            Item.useTime = 15;
+            Item.autoReuse = true;
+            Item.UseSound = SoundID.Item1;
+            Item.noMelee = true;
+            Item.noUseGraphic = true;
+
+            Item.shoot = Projectile;
+            Item.shootSpeed = 21f;
+            Item.damage = 80;
+            Item.knockBack = 4f;
+            Item.DamageType = DamageClass.Melee;
+        }
+
+        public override void AddRecipes() {
+            CreateRecipe()
+                .AddIngredient(ItemID.FragmentSolar, 18)
+                .AddTile(TileID.LunarCraftingStation)
+                .Register();
+        }
+
+        public int Projectile { get => ModContent.ProjectileType<WhiteDwarfProj>(); }
+
+        public override bool CanUseItem(Player player) => player.ownedProjectileCounts[Projectile] < player.GetModPlayer<BangarangPlayer>().ExtraBoomerangs + 1;
+    }
+}
---
+++ Content/Projectiles/Weapons/WhiteDwarfProj.cs
@@ -0,0 +1,143 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Bangarang.Content.Projectiles.Weapons {
+    public class WhiteDwarfProj : Boomerang {
+        public override void SetStaticDefaults() {
+            DisplayName.SetDefault("White Dwarf");
+        }
+
+        public override void SetDefaults() {
+            Projectile.width = 50;
+            Projectile.height = 50;
+            Projectile.aiStyle = -1;
+            Projectile.extraUpdates = 1;
+
+            Projectile.DamageType = DamageClass.Melee;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.penetrate = -1;
+            Projectile.tileCollide = true;
+
+            ReturnSpeed = 21f;
+            HomingOnOwnerStrength = 1.2f;
+            TravelOutFrames = 25;
+            Rotation = 0f;
+            DoTurn = true;
+        }
+
+        public override void AI() {
+            // If an enemy is nearby, apply daybroken
+            var nearbyNPCs = Helpers.GetNearbyEnemies(Projectile.Center, 3f * 16f, true, false);
+            foreach (var npc in nearbyNPCs) {
+                npc.AddBuff(BuffID.Daybreak, 60);
+            }
+
+            // Dust trail
+            for (int i = 0; i < 2; i++) {
+                Dust d = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.SolarFlare, Scale: Main.rand.NextFloat(0.8f, 1.2f));
+                d.noGravity = true;
+            }
+
+            // Point where we're travelling
+            Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
+
+            base.AI();
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity) {
+            Explode();
+
+            return base.OnTileCollide(oldVelocity);
+        }
+
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit) {
+            Explode();
+
+            base.OnHitNPC(target, damage, knockback, crit);
+        }
+
+        private void Explode() {
+            // Explosion
+            Projectile.NewProjectile(Projectile.GetSource_FromAI(), Projectile.Center.X, Projectile.Center.Y, 0f, 0f, ProjectileID.SolarWhipSwordExplosion, Projectile.damage, 10f, Projectile.owner, 0f, 0.85f + Main.rand.NextFloat() * 1.15f);
+
+            // Dust
+            int numDust = Main.rand.Next(4, 7);
+            for (int i = 0; i < numDust; i++) {
+                Dust dust = Dust.NewDustPerfect(Projectile.Center, DustID.SolarFlare);
+                if (Main.rand.NextBool(3)) {
+                    dust.noGravity = true;
+                    dust.velocity *= 8f;
+                    dust.scale = Main.rand.NextFloat(1.2f, 1.5f);
+                }
+                else {
+                    dust.noGravity = true;
+                    dust.velocity *= 2f;
+                    dust.scale = Main.rand.NextFloat(0.8f, 1.2f);
+                }
+            }
+        }
+
+        private Asset<Texture2D> _texture;
+        private Asset<Texture2D> _effect;
+        new private Asset<Texture2D> Texture {
+            get {
+                if (_texture == null) {
+                    _texture = ModContent.Request<Texture2D>("Bangarang/Content/Projectiles/Weapons/WhiteDwarfProj");
+                }
+                return _texture;
+            }
+        }
+        private Asset<Texture2D> Effect {
+            get {
+                if (_effect == null) {
+                    _effect = ModContent.Request<Texture2D>("Bangarang/Content/Projectiles/Weapons/WhiteDwarfEffect");
+                }
+                return _effect;
+            }
+        }
+
+        public override bool PreDraw(ref Color lightColor) {
+            Main.spriteBatch.End();
+            Main.spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.Additive, Main.Camera.Sampler, DepthStencilState.None, Main.Camera.Rasterizer, null, Main.Camera.GameViewMatrix.TransformationMatrix);
+
+            Main.spriteBatch.Draw(
+                Effect.Value,
+                Projectile.Center - Main.screenPosition + Projectile.velocity * -0.8f,
+                new Rectangle(0, 0, Effect.Width(), Effect.Height()),
+                GetColor(),
+                Projectile.rotation,
+                new Rectangle(0, 0, Effect.Width(), Effect.Height()).Size() / 2f,
+                Projectile.scale,
+                SpriteEffects.None,
+                0);
+
+            Main.spriteBatch.End();
+            Main.spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, Main.Camera.Sampler, DepthStencilState.None, Main.Camera.Rasterizer, null, Main.Camera.GameViewMatrix.TransformationMatrix);
+
+            Main.spriteBatch.Draw(
+                Texture.Value,
+                Projectile.Center - Main.screenPosition,
+                new Rectangle(0, 0, Texture.Width(), Texture.Height()),
+                Color.White,
+                Projectile.rotation,
+                new Rectangle(0, 0, Texture.Width(), Texture.Height()).Size() / 2f,
+                Projectile.scale,
+                SpriteEffects.None,
+                0);
+
+            return false;
+        }
+
+        private Color GetColor() {
+            float sineTime = MathF.Sin(Main.LocalPlayer.miscCounterNormalized);
+            Color color = Color.Lerp(Color.Orange, Color.OrangeRed, sineTime);
+            return color;
+        }
+    }
+}
