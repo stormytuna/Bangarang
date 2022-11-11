cbc7158001a7816549314ed81c45e40b2191f634
stormytuna <stormytuna@outlook.com>
2022-11-11 13:19:42 +0000

    Add mod call for registering a boomerang

--- Bangarang.cs
+++ Bangarang.cs
@@ -1,8 +1,30 @@
+using Bangarang.Common.Systems;
+using System;
 using Terraria.ModLoader;
 
-namespace Bangarang
-{
-	public class Bangarang : Mod
-	{
-	}
+namespace Bangarang {
+    public class Bangarang : Mod {
+        public override object Call(params object[] args) {
+            if (args is null) {
+                throw new ArgumentNullException(nameof(args), "Arguments cannot be null!");
+            }
+
+            if (args.Length == 0) {
+                throw new ArgumentException("Arguments cannot be empty!");
+            }
+
+            if (args[0] is int itemType) {
+                if (args[1] is int numBoomerangs) {
+                    ArraySystem.RegisterBoomerang(itemType, numBoomerangs);
+                    return true;
+                }
+                else {
+                    throw new Exception($"Expected an argument of type int, but got type {args[0].GetType().Name} instead");
+                }
+            }
+            else {
+                throw new Exception($"Expected an argument of type int, but got type {args[0].GetType().Name} instead");
+            }
+        }
+    }
 }
