4e110b93b35c3be7d6f72a8909c500126502407c
stormytuna <stormytuna@outlook.com>
2022-11-11 23:24:29 +0000

    Fix for arraysystem changes

--- Common/GlobalItems/BangarangGlobalItem.cs
+++ Common/GlobalItems/BangarangGlobalItem.cs
@@ -1,12 +1,14 @@
+using Bangarang.Common.Configs;
 using Bangarang.Common.Systems;
 using System.Linq;
 using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace Bangarang.Common.GlobalItems {
     public class BangarangGlobalItem : GlobalItem {
         public override void SetDefaults(Item item) {
-            if (ArraySystem.ProjectilesThatAreBoomerangs.Contains(item.shoot)) {
+            if (ArraySystem.ProjectilesThatAreBoomerangs.Contains(item.shoot) || (ServerConfig.Instance.AssumeModdedBoomerangs && ContentSamples.ProjectilesByType[item.shoot].aiStyle == 3 && item.CountsAsClass(DamageClass.Melee))) {
                 item.autoReuse = true;
             }
         }
